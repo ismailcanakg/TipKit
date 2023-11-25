495872fad3afb089040fcbc5f687a8f565cc149a
İsmail Can Akgün <ismailcanakg1@gmail.com>
2023-11-25 13:51:42 +0300

    Initial Commit

---
+++ TipKitProject/TipKitProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  TipKitProjectApp.swift
+//  TipKitProject
+//
+//  Created by İsmail Can Akgün on 25.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct TipKitProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
